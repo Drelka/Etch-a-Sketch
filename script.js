806ad62d0ee78f49b75717d1e86146b263c157a4
Drelka <47503020+Drelka@users.noreply.github.com>
2024-02-04 21:49:09 +0000

    Coloring with hover works now

--- script.js
+++ script.js
@@ -3,16 +3,37 @@
 // use loop to go 16 width and then 16 height for every width
 
 
-const container1 = document.querySelector(".container1");
-const container2 = document.querySelector(".container2");
+const container = document.querySelector(".container");
+const palette = document.querySelector(".palette");
+
+let colorPicked = "black"
+
+let className = 0;
+function classNames() {
+    for(let i = 1; i <= 10; i++) {
+        className += 1;
+        return className;
+    }
+}
+
+for(let a = 1; a <= 10; a++) {
+    const colorField = document.createElement("div");
+    colorField.id = `color-${classNames()}`;
+    palette.appendChild(colorField);
+}
 
 for(let i = 1; i <= 16; i++) {
     const squareW = document.createElement("div");
     squareW.className = "W";
-    container1.appendChild(squareW);
+    container.appendChild(squareW);
     for(let j = 1; j <= 16; j++){
         const squareH = document.createElement("div");
         squareH.className = "H";
         squareW.appendChild(squareH);
+
+        squareH.addEventListener("mouseover", () => {
+            squareH.style.backgroundColor = colorPicked;
+            squareH.style.borderColor = colorPicked;
+        });
     }
 }

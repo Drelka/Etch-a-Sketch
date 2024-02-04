0f783bef635df7254494d2645a27076fd4b7394c
Drelka <47503020+Drelka@users.noreply.github.com>
2024-02-04 22:42:29 +0000

    Made color pallet work

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
-// we got a div called container and want to create a grid inside 16x16 made of other divs
-
-// use loop to go 16 width and then 16 height for every width
-
 
 const container = document.querySelector(".container");
 const palette = document.querySelector(".palette");
 
-let colorPicked = "black"
+let colorPicked = "white"
 
-let className = 0;
-function classNames() {
+let idName = 0;
+function idNames() {
     for(let i = 1; i <= 10; i++) {
-        className += 1;
-        return className;
+        idName += 1;
+        return idName;
     }
 }
 
 for(let a = 1; a <= 10; a++) {
     const colorField = document.createElement("div");
-    colorField.id = `color-${classNames()}`;
+    colorField.className = "colors";
+    colorField.id = `color-${idNames()}`;
     palette.appendChild(colorField);
 }
 
+const darkslateblue = document.querySelector("#color-1");
+const royalblue = document.querySelector("#color-2");
+const deeppink = document.querySelector("#color-3");
+const yellow = document.querySelector("#color-4");
+const springgreen = document.querySelector("#color-5");
+const bisque = document.querySelector("#color-6");
+const aquamarine = document.querySelector("#color-7");
+const chartreuse = document.querySelector("#color-8");
+const orangered = document.querySelector("#color-9");
+const white = document.querySelector("#color-10");
+
+darkslateblue.style.backgroundColor = "darkslateblue";
+royalblue.style.backgroundColor = "royalblue";
+deeppink.style.backgroundColor = "deeppink";
+yellow.style.backgroundColor = "yellow";
+springgreen.style.backgroundColor = "springgreen";
+bisque.style.backgroundColor = "bisque";
+aquamarine.style.backgroundColor = "aquamarine";
+chartreuse.style.backgroundColor = "chartreuse";
+orangered.style.backgroundColor = "orangered";
+white.style.backgroundColor = "white";
+
+darkslateblue.addEventListener("click", () => {colorPicked = darkslateblue.style.backgroundColor});
+royalblue.addEventListener("click", () => {colorPicked = royalblue.style.backgroundColor});
+deeppink.addEventListener("click", () => {colorPicked = deeppink.style.backgroundColor});
+yellow.addEventListener("click", () => {colorPicked = yellow.style.backgroundColor});
+springgreen.addEventListener("click", () => {colorPicked = springgreen.style.backgroundColor});
+bisque.addEventListener("click", () => {colorPicked = bisque.style.backgroundColor});
+aquamarine.addEventListener("click", () => {colorPicked = aquamarine.style.backgroundColor});
+chartreuse.addEventListener("click", () => {colorPicked = chartreuse.style.backgroundColor});
+orangered.addEventListener("click", () => {colorPicked = orangered.style.backgroundColor});
+white.addEventListener("click", () => {colorPicked = white.style.backgroundColor});
+
+
 for(let i = 1; i <= 16; i++) {
     const squareW = document.createElement("div");
-    squareW.className = "W";
     container.appendChild(squareW);
     for(let j = 1; j <= 16; j++){
         const squareH = document.createElement("div");
-        squareH.className = "H";
+        squareH.className = "squares";
         squareW.appendChild(squareH);
 
         squareH.addEventListener("mouseover", () => {
